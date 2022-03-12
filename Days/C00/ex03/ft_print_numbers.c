959504d247b18f8c0e44e941b175afef2f4d2ec1
Ayman S15 <aymanbouabra@gmail.com>
2022-03-12 14:27:38 +0100

    ft_print_numbers

---
+++ Days/C00/ex03/ft_print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+void ft_print_numbers(){
+    
+    char a = '0';
+    while (a <= '9'){
+        write(1,&a,1);
+        a++;
+    }
+    
+}
+
+int main (){
+
+    ft_print_numbers();
+    return 0;
+}
